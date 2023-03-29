0136426c67a662b10107130c82248e3659360b29
thehamcar <128240150+thehamcar@users.noreply.github.com>
2023-03-29 22:29:50 +0800

    Update RequestIO.java

--- Source Codes/src/fypms/RequestIO.java
+++ Source Codes/src/fypms/RequestIO.java
@@ -1,110 +1,61 @@
-package fypms;
+package projectandreq;
 
 import java.io.*;
 import java.util.*;
 import java.util.Scanner;
+
+import projectandreq.Request.requestType;
+import projectandreq.Project;
+
 import java.util.ArrayList;
 
-public class RequestIO {
-	private File requestFile = new File("Database/request_list.txt");
-	
-	private static ArrayList<Request> requests = new ArrayList<>();
-	
-	public static ArrayList<Request> readRequests() throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(requestFile))) {
-            String line;
-            boolean firstLine = true;
-            
-            while ((line = reader.readLine()) != null) {
-                if (firstLine) {
-                    firstLine = false;
-                    continue;
-                }
-                
-                String[] tokens = line.split(";");
-                int requestID = Integer.parseInt(tokens[0]);
-                RequestType type = RequestType.valueOf(tokens[1]);
-                String sender = tokens[2];
-                String receiver = tokens[3];
-                int projectID = Integer.parseInt(tokens[4]);
-				RequestStatus status = RequestStatus.valueOf(tokens[5]);
-                
-                Request request;
 
-				switch(type) {
-					case ALLOCATION:
-						request = new RequestForAllocation(type, sender, receiver, projectID);
-						break;
-					case DEREGISTRATION:
-						request = new RequestForDeregistration(type, sender, receiver, projectID);
-						break;
-					case TITLECHANGE:
-						request = new RequestForTitleChange(type, sender, receiver, projectID);
-						break;
-					case TRANSFER:
-						request = new RequestForTransfer(type, sender, receiver, projectID);
-						break;
-					default:
-						throw new IllegalArgumentException("Invalid request type: " + type);
-				}
-				
-                request.setRequestID(requestID);
-				request.setStatus(status);
-                
-                requests.add(request);
-            }
-        }
-        
-        return requests;
+public class RequestIO {
+  public static final String SEPARATOR = "|";
+  
+  //private File requestToSupervisorFile = new File("RequestsToSupervisor.txt");
+  //private File requestToCoordinatorFile = new File("RequestsToCoordinator.txt");
+  
+  public void readRequest(String RequestsFile) throws IOException {
+    ArrayList SupervisorRequests = (ArrayList)read("requestToSupervisorFile");
+    ArrayList requests = new ArrayList();
+    // Request(String senderID, requestType type, String receiverID, Project project)
+    for(int i = 0;i<SupervisorRequests.size();i++) {
+      String st = (String)SupervisorRequests.get(i);
+      String[] tokens = st.split("|");
+      
+      Request request = new Request(tokens[0], tokens[1], requestType.valueOf(tokens[2]), tokens[3]);
+      
+      //how to create request object
+    } 
+    
+  }
+  
+
+  public void writeRequest(List data) throws IOException {
+    PrintWriter out = new PrintWriter(new FileWriter("RequestsFile"));
+    try {
+      for(int i=0;i<data.size();i++) {
+        out.println((String)data.get(i));
+      }
     }
-    
-    public static void writeRequest(Request request) throws IOException {
-		BufferedWriter writer = new BufferedWriter(new FileWriter(requestFile, true));
-		int requestID = requests.size() + 1; 
-		
-		String line = requestID + ";" + request.getType() + ";" + request.getSender() + ";" + request.getReceiver() + ";" + request.getProjectID() + ";" + request.getStatus();
-		
-		writer.newLine();
-		writer.write(line);
-		writer.close();
-	}
-	
-    
-    public static void modifyRequest(Request request) throws IOException {
-		int requestID = project.getRequestID();
-		
-		String newLine = requestID + ";" + request.getType() + ";" + request.getSender() + ";" + request.getReceiver() + ";" + request.getProjectID() + ";" + request.getStatus();
-		
-		// Create temporary file
-		File tempFile = new File(requestFile.getAbsolutePath() + ".tmp");
-		BufferedReader reader = new BufferedReader(new FileReader(requestFile));
-		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-		
-		// Read each line and modify the line corresponding to the project ID
-		String line;
-		while ((line = reader.readLine()) != null) {
-			if (line.startsWith(String.valueOf(requestID))) {
-				writer.write(newLine);
-			} else {
-				writer.write(line);
-			}
-			writer.newLine();
-		}
-		
-		reader.close();
-		writer.close();
-		
-		// Replace project file with temporary file
-		if (!requestFile.delete()) {
-			System.out.println("Failed to delete original file");
-			return;
-		}
-		
-		if (!tempFile.renameTo(requestFile)) {
-			System.out.println("Failed to rename temporary file");
-			return;
-		}
-	}
-		
-		
+    finally {
+      out.close();
+    }
+  }
+  
+  public static List read(String filename) throws IOException{
+    List requests = new ArrayList();
+    Scanner scanner = new Scanner(new FileInputStream(filename));
+    try {
+      while(scanner.hasNextLine()) {
+        requests.add(scanner.nextLine());
+      }
+    }
+    finally {
+      scanner.close();
+    }
+    return requests;
+  }
+
 }
