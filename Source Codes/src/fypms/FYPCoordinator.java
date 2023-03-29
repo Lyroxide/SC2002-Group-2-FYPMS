fe210fc9c16ce3d23f8f13368c48582235c09821
lim-jun-yu <128129499+lim-jun-yu@users.noreply.github.com>
2023-03-29 17:46:26 +0800

    Update FYPCoordinator.java

--- Source Codes/src/fypms/FYPCoordinator.java
+++ Source Codes/src/fypms/FYPCoordinator.java
@@ -1,6 +1,7 @@
 package sc2002_assignment;
 
 import java.util.ArrayList;
+import java.util.*;
 import java.util.Collections;
 
 public class FYPCoordinator extends Supervisor {
@@ -8,27 +9,35 @@ public class FYPCoordinator extends Supervisor {
 	private String coordinatorID;
 	
 	// viewProjects(): changed status (argument) to typeOfProjects, added ArrayList<Project> (argument)
+	// viewPendingReques(): under Request class, add requestStatus
 	
 	public FYPCoordinator(String coordinatorID) {
 		this.coordinatorID = coordinatorID;
 	}
 	
-	public void changeSupervisor(Project project, String supervisorID) {
-		project.setSupervisorID(supervisorID);
+	public void changeSupervisor(Supervisor s1, Supervisor s2, Project project) {
+		for (int i = 0; i < 2; i++) {
+			if (s1.getProjectsSupervising.get(i).getProjectID == project.getProjectID) {
+				s1.getProjectsSupervising.get(i).setProjectID(null);
+				s2.getProjectsSupervising.add(project);
+				project.setSupervisorID(s2.getSupervisorID);
+			}
+		}
 	}
 	
-	public void allocateProject(Student student, Project project) {
-		student.setCurProject(project);
+	public void allocateProject(Project project, Student student) {
+		student.setCurProject(project.getProjectID);
+		project.setStudentID(student.getStudentID);
 	}
 	
-	public void deregisterStudent(Student student, Project project) {
-		project.setStudentID(null);
-		project.setStatus("Available");
+	public void deregisterStudent(Project project, Student student) {
 		student.setCurProject(null);
 		student.setProjRegistered(false);
+		project.setProjectStatus("Available");
+		project.setStudentID(null);
 	}
 	
-	public void viewProjects(ArrayList<Project> project, int typeOfProject) {
+	/*public void viewProjects(ArrayList<Project> project, int typeOfProject) {
 		// in the argument:
 		// ArrayList<Project> project: array list of project class
 		// int typeOfProject: the type of project to be displayed
@@ -46,14 +55,14 @@ public class FYPCoordinator extends Supervisor {
 		
 		if (type == "All") {
 			for (int i = 0; i < project.size(); i++) {
-				System.out.println(index + "\t" + project.get(i).projectID + "\t" + project.get(i).projectTitle);
+				System.out.println(index + "\t" + project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
 				index++;
 			}
 		}
 		else {
 			for (int i = 0; i < project.size(); i++) {
 				if (project.get(i).status == type) {
-					System.out.println(index + "\t" + project.get(i).projectID + "\t" + project.get(i).projectTitle);
+					System.out.println(index + "\t" + project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
 					index++;
 				}
 			}
@@ -70,111 +79,161 @@ public class FYPCoordinator extends Supervisor {
 		// studentID
 		// projectID
 		// projectTitle
-	}
+	}*/
 	
-	public void generateReport(ArrayList<Project> project, int filterType, int noOfSupervisors) {
+	public void generateReport(ArrayList<Project> project, ArrayList<Supervisor> supervisor, ArrayList<Student> student, int filterType) {
 		// in the argument:
 		// ArrayList<Project> project: array list of project class
-		// int filterType: filter based on ?
-		
-		/*
-		ArrayList<String> tempArray = new ArrayList<>();
-		
-		switch (filterType) {
-			case 1: {
-				System.out.println("Project Detail Report (filtered by Project Status)");
-				System.out.println("Status\tSupervisor ID\tStudent ID\tProject ID\tProject Title");
-				tempArray.add("Available");
-				tempArray.add("Unavailable");
-				tempArray.add("Reserved");
-				tempArray.add("Allocated");
-				
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < project.size(); j++) {
-						if (project.get(i).status == tempArray.get(i)) {
-							System.out.println(project.get(j).status + "\t" + project.get(j).supervisorID + "\t" + project.get(j).studentID + 
-									"\t" + project.get(j).projectID + "\t" + project.get(j).projectTitle);
-						}
-					}
-				}
-				break;
-			}
-			case 2: {
-				System.out.println("Project Detail Report (filtered by Supervisor)");
-				System.out.println("Supervisor ID\tStudent ID\tProject ID\tProject Title\tStatus");
-				
-				
-				for (int i = 0; i < noOfSupervisors; i++) { // loop count: no. of supervisors
-					for (int k = 0; k < project.size(); k++) { // if ID is not in tempArray, add in tempArray
-						if (tempArray.size() == 0 || project.get(k).supervisorID != tempArray.get(tempArray.size() - 1)) {
-							tempArray.add(project.get(k).supervisorID);
-							break;
-						}
-					}
-					for (int j = 0; j < project.size(); j++) { // if ID equals last of tempArray, print
-						if (project.get(j).supervisorID == tempArray.get(tempArray.size() - 1)) {
-							System.out.println(project.get(j).supervisorID + "\t" + project.get(j).studentID + 
-									"\t" + project.get(j).projectID + "\t" + project.get(j).projectTitle + "\t" + project.get(j).status);
-						}
-						
-					}
-				}
-				break;
-			}
-			case 3: {
-				
-				break;
-			}
-		}*/
-		
-		
-		// to be added in Project class for sorting with Comparable
-		/*
-		public int compareTo(Project project, int filterType) {
-			if (filterType == 1) return this.status.compareTo(project.status);
-			else if (filterType == 2) return this.supervisorName.compareTo(project.supervisorName);
-			else if (filterType == 3) return this.studentName.compareTo(project.studentName);
-			else return this.projectID - project.projectID;
-		}*/
+		int choice, index;
+		String type;
+		Scanner read = new Scanner(System.in);
 		ProjectFilter p = new ProjectFilter();
 		
-		Collections.sort(project, p.filterType(1));
 		if (filterType == 1) {
-			Collections.sort(project, p.filterType(1));
-			System.out.println("Project Detail Report (filtered by Project Status)");
-			System.out.println("Status\tSupervisor name\tStudent name\tProject ID\tProject Title");
-			for (int i = 0; i < project.size(); i++) {
-				System.out.println(project.get(i).status + "\t" + project.get(i).supervisorName + "\t" + project.get(i).studentName + 
-						"\t" + project.get(i).projectID + "\t" + project.get(i).projectTitle);
+			System.out.println("Generate report by: STATUS");
+			System.out.println("Select Project Status to view:");
+			System.out.println("(1) Available");
+			System.out.println("(2) Unavailable");
+			System.out.println("(3) Reserved");
+			System.out.println("(4) Allocated");
+			System.out.println("(5) All");
+			
+			do {
+				System.out.println("Please select a choice:");
+				choice = read.nextInt();
+				if (choice < 0 || choice > 5) System.out.println("Invalid choice!");
+			} while (choice > 0 && choice < 6);
+			
+			if (choice == 1) type = "Available";
+			else if (choice == 2) type = "Unavailable";
+			else if (choice == 3) type = "Reserved";
+			else if (choice == 4) type = "Allocated";
+			else type = "All";
+			
+			System.out.println(type + " projects:");
+			System.out.println("Student\tSupervisor\tStatus\tProject ID\tProject Title");
+			
+			index = 1;
+			if (type == "All") {
+				Collections.sort(project, p.filterType(1));
+				for (int i = 0; i < project.size(); i++) {
+					System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
+							"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
+					index++;
+				}
 			}
+			else {
+				for (int i = 0; i < project.size(); i++) {
+					if (project.get(i).status == type) {
+						System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
+								"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
+						index++;
+					}
+				}
+			}
+			
 		}
 		else if (filterType == 2) {
-			Collections.sort(project, p.filterType(2));
-			System.out.println("Project Detail Report (filtered by Supervisor)");
-			System.out.println("Supervisor Name\tStudent Name\tProject ID\tProject Title\tStatus");
-			for (int i = 0; i < project.size(); i++) {
-				System.out.println(project.get(i).supervisorName + "\t" + project.get(i).studentName + 
-						"\t" + project.get(i).projectID + "\t" + project.get(i).projectTitle + "\t" + project.get(i).status);
+			System.out.println("Generate report by: SUPERVISOR");
+			System.out.println("Select Supervisor to view:");
+			for (int i = 0; i < supervisor.size(); i++) {
+				System.out.println("(" + (i + 1) + ")" + " " + supervisor.get(i).getSupervisorName);
+			}
+			System.out.println("(" + (supervisor.size() + 1) + ") All" );
+			
+			do {
+				System.out.println("Please select a choice:");
+				choice = read.nextInt();
+				if (choice < 0 || choice > (supervisor.size() + 1)) System.out.println("Invalid choice!");
+			} while (choice < 0 || choice > (supervisor.size() + 1));
+			
+			index = 1;
+			if (choice == (supervisor.size() + 1)) {
+				Collections.sort(project, p.filterType(2));
+				for (int i = 0; i < project.size(); i++) {
+					System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
+							"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
+					index++;
+				}
+			}
+			else {
+				for (int i = 0; i < project.size(); i++) {
+					if (project.get(i).getSupervisorName == supervisor.get(choice - 1).getSupervisorName) {
+						System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
+								"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
+						index++;
+					}
+				}
 			}
 			
 		}
 		else if (filterType == 3) {
-			Collections.sort(project, p.filterType(3));
-			System.out.println("Project Detail Report (filtered by Student)");
-			System.out.println("Student Name\tSupervisor Name\tProject ID\tProject Title\tStatus");
-			for (int i = 0; i < project.size(); i++) {
-				System.out.println(project.get(i).studentName + "\t" + project.get(i).supervisorName + 
-						"\t" + project.get(i).projectID + "\t" + project.get(i).projectTitle + "\t" + project.get(i).status);
+			System.out.println("Generate report by: STUDENT");
+			System.out.println("Select Student to view:");
+			for (int i = 0; i < student.size(); i++) {
+				System.out.println("(" + (i + 1) + ")" + " " + student.get(i).getStudentName);
 			}
+			System.out.println("(" + (student.size() + 1) + ") All" );
+			
+			do {
+				System.out.println("Please select a choice:");
+				choice = read.nextInt();
+				if (choice < 0 || choice > (student.size() + 1)) System.out.println("Invalid choice!");
+			} while (choice < 0 || choice > (student.size() + 1));
+			
+			index = 1;
+			if (choice == (student.size() + 1)) {
+				Collections.sort(project, p.filterType(3));
+				for (int i = 0; i < project.size(); i++) {
+					System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
+							"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
+					index++;
+				}
+			}
+			else {
+				for (int i = 0; i < project.size(); i++) {
+					if (project.get(i).getStudentName == student.get(choice - 1).getStudentName) {
+						System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
+								"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
+						break;
+					}
+				}
+			}
+			
 		}
 		else {
-			Collections.sort(project, p.filterType(4));
-			System.out.println("Project Detail Report (filtered by Project)");
-			System.out.println("Project ID\tProject Title\tSupervisor Name\tStudent Name\tStatus");
+			System.out.println("Generate report by: PROJECT");
+			System.out.println("Select Project to view:");
 			for (int i = 0; i < project.size(); i++) {
-				System.out.println(project.get(i).projectID + "\t" + project.get(i).projectTitle + 
-						"\t" + project.get(i).supervisorName + "\t" + project.get(i).studentName + "\t" + project.get(i).status);
+				System.out.println("(" + (i + 1) + ")" + " " + project.get(i).getProjectTitle);
 			}
+			System.out.println("(" + (project.size() + 1) + ") All" );
+			
+			do {
+				System.out.println("Please select a choice:");
+				choice = read.nextInt();
+				if (choice < 0 || choice > (project.size() + 1)) System.out.println("Invalid choice!");
+			} while (choice < 0 || choice > (project.size() + 1));
+			
+			index = 1;
+			if (choice == (project.size() + 1)) {
+				Collections.sort(project, p.filterType(4));
+				for (int i = 0; i < project.size(); i++) {
+					System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
+							"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
+					index++;
+				}
+			}
+			else {
+				for (int i = 0; i < project.size(); i++) {
+					if (project.get(i).getProjectID == project.get(choice - 1).getProjectID) {
+						System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
+								"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
+						break;
+					}
+				}
+			}
+			
 		}
 		
 		// case 1: filter by status
@@ -183,15 +242,29 @@ public class FYPCoordinator extends Supervisor {
 		// case 4: filter by project id
 	}
 	
-	public ArrayList<Request> viewPendingRequest() {
-		// under Request class, change requestType to requestStatus?
+	public void viewPendingRequest(ArrayList<Request> request) {
+		int index = 1;
+		
+		System.out.println("Pending requests");
+		System.out.println("Request type\tSender\tReceiver");
+		for (int i = 0; i < request.size(); i++) {
+			if (request.get(i).requestStatus == "Pending") {
+				System.out.println(index + "\t" + request.get(i).getRequestType + "\t" + request.get(i).getSender + "\t" + 
+			request.get(i).getReveiver);
+				index++;
+			}
+		}
 	}
 	
-	public ArrayList<Request> viewRequestHistory() {
+	public void viewRequestHistory(ArrayList<Request> request) {
+		int index = 1;
 		
-	}
-	
-	public void filter() {
-		
+		System.out.println("Requests history");
+		System.out.println("Request type\tSender\tReceiver\tStatus");
+		for (int i = 0; i < request.size(); i++) {
+			System.out.println(index + "\t" + request.get(i).getRequestType + "\t" + request.get(i).getSender + "\t" + 
+		request.get(i).getReveiver + "\t" + request.get(i).getStatus);
+			index++;
+		}
 	}
 }
