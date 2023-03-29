926dba1039ac1747f4eca31f0cd3e147b62e7fcf
thehamcar <128240150+thehamcar@users.noreply.github.com>
2023-03-29 22:31:37 +0800

    Update Request.java

--- Source Codes/src/fypms/Request.java
+++ Source Codes/src/fypms/Request.java
@@ -1,81 +1,65 @@
-package fypms;
+package projectandreq;
 
-public abstract class Request {
-  enum RequestType {
-      ALLOCATION, DEREGISTRATION, TITLECHANGE, TRANSFER
+import projectandreq.Project.Status;
+
+/
+ * Request represents a request object being sent from one user to another
+ */
+
+public class Request {
+  public enum requestType{
+    deregister, allocate, changeTitle, transferStudent
   }
-  enum RequestStatus {
-	  PENDING, APPROVED, REJECTED
+  private requestType type;
+  private User sender;
+  private User receiver;
+  private Project project;
+  
+  /
+   * Constructor that sets the request's senderID, receiverID, and requestType
+   * @param senderID ID of requester
+   * @param type request type
+   * @param receiverID ID of receiver
+   */
+  public Request(String senderID, String receiverID, requestType type, Project project) {
+    this.type = type;
+    this.sender.setName(senderID);
+    this.receiver.setName(receiverID);
+    this.project = project;
+    //RequestIO.readRequest()
   }
-  
-  private RequestType type;
-  private RequestStatus status;
-  private String sender;
-  private String receiver;
-  private int requestID;
-  private int projectID;
-  
-  
-  public Request(RequestType type, String sender, String receiver, int projectID, RequestStatus status) {
-	  this.type = type;
-      this.sender = sender;
-      this.receiver = receiver;
-      this.projectID = projectID;
-	  this.status = status;
-  }
-  
-  public void setType(RequestType type) {
-      this.type = type;
-  }
-  
-  public RequestType getType() {
-	  return type;
-  }
-  
-  public void setStatus(RequestStatus status) {
-      this.status = status;
-  }
-  
-  public RequestStatus getStatus() {
-	  return status;
-  }
-  
-  public void setSender(String user) {
-      this.sender = user;
-  }
-  
-  public String getSender() {
-	  return sender;
-  }
-  
-  public void setReceiver(String user) {
-      this.receiver = user;
-  }
-  
-  public String getReceiver() {
-	  return receiver;
-  }
-  
-  public void setProjectID(int projectID) {
-	  this.projectID = projectID;
+
+  public void Approve(requestType type, Project project) {
+    if(type == type.allocate)
+    {
+      
+      project.projectStatus = Status.allocated;
+      //set student ID 
+    
+    }
+    
+    if(type == type.deregister)
+    {
+      
+      project.projectStatus = Status.available;
+      project.studentID = null;
+    }
+    
+    if(type == type.changeTitle) 
+    {
+      
+      project.setProjectTitle();
+    }
+    
+    if(type == type.transferStudent)
+    {
+      FYPC.changeSupervisor(Project project, String supervisorID);
+    }
   }
   
-  public int getProjectID() {
-	  return projectID;
+  public void Reject() {
+    System.out.println("Request was rejected");
   }
   
-  public void setRequestID() {
-	  this.requestID = requestID;
-  }
   
-  public int getRequestID() {
-	  return requestID;
-  }
-  
-  public abstract void approve(String param);
-  
-  public void reject() {
-	  setStatus(RequestStatus.REJECTED);
-	  System.out.println("You have successfully rejected the request.")
-  }
 }
