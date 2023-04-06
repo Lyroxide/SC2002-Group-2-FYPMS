f3717b68f4e3307ab0c0f7a392ec13e1d1dc3002
lim-jun-yu <128129499+lim-jun-yu@users.noreply.github.com>
2023-04-07 00:39:29 +0800

    Update FYPCoordinator.java
    updated generateReport()

--- Source Codes/src/fypms/FYPCoordinator.java
+++ Source Codes/src/fypms/FYPCoordinator.java
@@ -1,4 +1,4 @@
-package sc2002_assignment;
+package fypms;
 
 import java.util.ArrayList;
 import java.util.*;
@@ -111,12 +111,15 @@ public class FYPCoordinator extends Supervisor {
 		}
 	}
 	
-	/*public void generateReport(int filterType) {
+	public void generateReport(int filterType) {
 		
 		int choice, i = 0;
 		Scanner read = new Scanner(System.in);
 		
 		if (filterType == 1) {
+			ArrayList<Project> proj = new ArrayList<>();
+			proj = ProjectIO.readProjects();
+			
 			System.out.println("Generate report by: STATUS");
 			System.out.println("Select Project Status to view:");
 			System.out.println("(1) Available");
@@ -131,185 +134,114 @@ public class FYPCoordinator extends Supervisor {
 				if (choice < 0 || choice > 5) System.out.println("Invalid choice!");
 			} while (choice < 0 || choice > 6);
 			
-			ArrayList<Project> proj = new ArrayList<>();
-			proj = ProjectIO.readProjects();
-			
 			if (choice == 1) {
 				while (i < proj.size()) {
 					if (proj.get(i).getStatus != Status.AVAILABLE)  proj.remove(i);
 					else i++;
 				}
 			}
-			
-		}
-	}
-	
-	
-	public void generateReport(int filterType) {
-		// old one
-		int choice, index;
-		String type;
-		Scanner read = new Scanner(System.in);
-		ProjectFilter p = new ProjectFilter();
-		
-		// argument take in filter type only
-		// read in various file
-		// perform filter
-		// return array list
-		
-		
-		if (filterType == 1) {
-			System.out.println("Generate report by: STATUS");
-			System.out.println("Select Project Status to view:");
-			System.out.println("(1) Available");
-			System.out.println("(2) Unavailable");
-			System.out.println("(3) Reserved");
-			System.out.println("(4) Allocated");
-			System.out.println("(5) All");
-			
-			do {
-				System.out.println("Please select a choice:");
-				choice = read.nextInt();
-				if (choice < 0 || choice > 5) System.out.println("Invalid choice!");
-			} while (choice > 0 && choice < 6);
-			
-			if (choice == 1) type = "Available";
-			else if (choice == 2) type = "Unavailable";
-			else if (choice == 3) type = "Reserved";
-			else if (choice == 4) type = "Allocated";
-			else type = "All";
-			
-			System.out.println(type + " projects:");
-			System.out.println("Student\tSupervisor\tStatus\tProject ID\tProject Title");
-			
-			index = 1;
-			if (type == "All") {
-				Collections.sort(project, p.filterType(1));
-				for (int i = 0; i < project.size(); i++) {
-					System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
-							"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
-					index++;
+			else if (choice == 2) {
+				while (i < proj.size()) {
+					if (proj.get(i).getStatus != Status.UNAVAILABLE)  proj.remove(i);
+					else i++;
 				}
 			}
-			else {
-				for (int i = 0; i < project.size(); i++) {
-					if (project.get(i).status == type) {
-						System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
-								"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
-						index++;
-					}
+			else if (choice == 3) {
+				while (i < proj.size()) {
+					if (proj.get(i).getStatus != Status.RESERVED)  proj.remove(i);
+					else i++;
+				}
+			}
+			else if (choice == 4) {
+				while (i < proj.size()) {
+					if (proj.get(i).getStatus != Status.ALLOCATED)  proj.remove(i);
+					else i++;
 				}
 			}
 			
+			return proj;
 		}
 		else if (filterType == 2) {
+			
+			ArrayList<Supervisor> sup = new ArrayList<>();
+			sup = UserIO.readSupervisors();
+			
 			System.out.println("Generate report by: SUPERVISOR");
 			System.out.println("Select Supervisor to view:");
-			for (int i = 0; i < supervisor.size(); i++) {
-				System.out.println("(" + (i + 1) + ")" + " " + supervisor.get(i).getSupervisorName);
+			for (i = 0; i < sup.size(); i++) {
+				System.out.println("(" + i + 1 + ")" + sup.get(i).getName());
 			}
-			System.out.println("(" + (supervisor.size() + 1) + ") All" );
+			System.out.println("(" + sup.size() + 1 + ") All");
 			
 			do {
 				System.out.println("Please select a choice:");
 				choice = read.nextInt();
-				if (choice < 0 || choice > (supervisor.size() + 1)) System.out.println("Invalid choice!");
-			} while (choice < 0 || choice > (supervisor.size() + 1));
+				if (choice < 0 || choice > sup.size()) System.out.println("Invalid choice!");
+			} while (choice < 0 || choice > sup.size());
 			
-			index = 1;
-			if (choice == (supervisor.size() + 1)) {
-				Collections.sort(project, p.filterType(2));
-				for (int i = 0; i < project.size(); i++) {
-					System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
-							"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
-					index++;
+			if (choice <= sup.size()) {
+				i = 0;
+				while (i < sup.size()) {
+					if (sup.get(i).getName() != sup.get(choice - 1).getName()) sup.remove(i);
+					else i++;
 				}
 			}
-			else {
-				for (int i = 0; i < project.size(); i++) {
-					if (project.get(i).getSupervisorName == supervisor.get(choice - 1).getSupervisorName) {
-						System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
-								"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
-						index++;
-					}
-				}
-			}
-			
+			return sup;
 		}
 		else if (filterType == 3) {
+			
+			ArrayList<Student> stu = new ArrayList<>();
+			stu = UserIO.readStudents();
+			
 			System.out.println("Generate report by: STUDENT");
 			System.out.println("Select Student to view:");
-			for (int i = 0; i < student.size(); i++) {
-				System.out.println("(" + (i + 1) + ")" + " " + student.get(i).getStudentName);
+			for (i = 0; i < stu.size(); i++) {
+				System.out.println("(" + i + 1 + ")" + stu.get(i).getName());
 			}
-			System.out.println("(" + (student.size() + 1) + ") All" );
+			System.out.println("(" + stu.size() + 1 + ") All");
 			
 			do {
 				System.out.println("Please select a choice:");
 				choice = read.nextInt();
-				if (choice < 0 || choice > (student.size() + 1)) System.out.println("Invalid choice!");
-			} while (choice < 0 || choice > (student.size() + 1));
+				if (choice < 0 || choice > stu.size()) System.out.println("Invalid choice!");
+			} while (choice < 0 || choice > stu.size());
 			
-			index = 1;
-			if (choice == (student.size() + 1)) {
-				Collections.sort(project, p.filterType(3));
-				for (int i = 0; i < project.size(); i++) {
-					System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
-							"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
-					index++;
+			if (choice <= stu.size()) {
+				i = 0;
+				while (i < stu.size()) {
+					if (stu.get(i).getName() != stu.get(choice - 1).getName()) stu.remove(i);
+					else i++;
 				}
 			}
-			else {
-				for (int i = 0; i < project.size(); i++) {
-					if (project.get(i).getStudentName == student.get(choice - 1).getStudentName) {
-						System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
-								"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
-						break;
-					}
-				}
-			}
-			
+			return stu;
 		}
 		else {
-			System.out.println("Generate report by: PROJECT");
-			System.out.println("Select Project to view:");
-			for (int i = 0; i < project.size(); i++) {
-				System.out.println("(" + (i + 1) + ")" + " " + project.get(i).getProjectTitle);
+			ArrayList<Project> proj = new ArrayList<>();
+			proj = ProjectIO.readProjects();
+			
+			System.out.println("Generate report by: PROJECTS");
+			System.out.println("Select Projects to view:");
+			for (i = 0; i < proj.size(); i++) {
+				System.out.println("(" + i + 1 + ")" + proj.get(i).getName());
 			}
-			System.out.println("(" + (project.size() + 1) + ") All" );
+			System.out.println("(" + proj.size() + 1 + ") All");
 			
 			do {
 				System.out.println("Please select a choice:");
 				choice = read.nextInt();
-				if (choice < 0 || choice > (project.size() + 1)) System.out.println("Invalid choice!");
-			} while (choice < 0 || choice > (project.size() + 1));
+				if (choice < 0 || choice > proj.size()) System.out.println("Invalid choice!");
+			} while (choice < 0 || choice > proj.size());
 			
-			index = 1;
-			if (choice == (project.size() + 1)) {
-				Collections.sort(project, p.filterType(4));
-				for (int i = 0; i < project.size(); i++) {
-					System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
-							"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
-					index++;
+			if (choice <= proj.size()) {
+				i = 0;
+				while (i < proj.size()) {
+					if (proj.get(i).getName() != proj.get(choice - 1).getName()) proj.remove(i);
+					else i++;
 				}
 			}
-			else {
-				for (int i = 0; i < project.size(); i++) {
-					if (project.get(i).getProjectID == project.get(choice - 1).getProjectID) {
-						System.out.println(index + "\t" + project.get(i).getStudentName + "\t" + project.get(i).getSupervisorName + 
-								"\t" + project.get(i).getStatus + "\t" + project.project.get(i).getProjectID + "\t" + project.get(i).getProjectTitle);
-						break;
-					}
-				}
-			}
-			
+			return proj;
 		}
-		
-		// case 1: filter by status
-		// case 2: filter by supervisor
-		// case 3: filter by student id
-		// case 4: filter by project id
-	}*/
+	}
 	
 	public ArrayList<Request> viewPendingRequest() {
 		ArrayList<Request> req = new ArrayList<>();
